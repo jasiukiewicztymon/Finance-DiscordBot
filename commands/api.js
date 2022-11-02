6861bb61f539b775c0a8b2256c5c9adc9ae9862e
Jasiukiewicz Tymon <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-11-02 17:33:28 +0100

    ✨ - Updated API command

--- commands/api.js
+++ commands/api.js
@@ -1,9 +1,27 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('api')
-		.setDescription('✨ About api embed message!'),
+		.setDescription('✨ About API embed message!'),
 	async execute(interaction) {
+        const authorEmbed = new EmbedBuilder()
+            .setTitle('API information')
+            .setColor(0x06D6A0)
+            .setDescription('Here you can found some information about the API ✨')
+            .addFields(
+                { name: 'Why Yahoo finance API', value: 'We have choose the Yahoo finance API, because it\'s free and have a relative good docs and easy in use' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'More information', value: 'You can get more information, clicking the button below' },
+            );
+        const buttons = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setLabel('More')
+					.setStyle(ButtonStyle.Link)
+                    .setURL('https://www.yahoofinanceapi.com/'),
+			);
+
+        await interaction.reply({ embeds: [ authorEmbed ], components: [ buttons ] });
 	},
 };
